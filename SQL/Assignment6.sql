0de8b77c5e82192ab418cf409447e0e91bee4804
luantrong.nguyen <luantrong.nguyen@bytedance.com>
2025-10-29 20:51:45 +0800

    vc

--- SQL/Assignment6.sql
+++ SQL/Assignment6.sql
@@ -1,27 +1,51 @@
-USE `VTI_Department_DB`;
+-- USE `VTI_Department_DB`;
 
+-- DELIMITER $$
+
+-- CREATE PROCEDURE GetAccountsInDepartment(IN DepartmentNameIn VARCHAR(200))
+-- BEGIN
+--     SELECT AccountID, Email, Username, FullName FROM `Account`
+--     WHERE AccountID IN (
+--         SELECT DepartmentID FROM `Department`
+--         WHERE DepartmentName = DepartmentNameIn
+--     );
+-- END $$
+
+-- CALL GetAccountsInDepartment('Engineer');
+-- DELIMITER ;
+
+-- DELIMITER $$
+-- CREATE PROCEDURE GetAccountPerGroup()
+-- BEGIN
+-- 	SELECT G.GroupID as GID, G.GroupName as GroupName, COUNT(AccountID) as NumAccount
+--     FROM `Group` G LEFT JOIN `GroupAccount` GA ON G.GroupID = GA.GroupID
+-- 	GROUP BY G.GroupID;
+-- END $$
+
+-- CALL GetAccountPerGroup();
+
+-- DELIMITER ;
+
+
+
+
+DROP PROCEDURE TypeQuestionStatsThisMonth;
 DELIMITER $$
-
-CREATE PROCEDURE GetAccountsInDepartment(IN DepartmentNameIn VARCHAR(200))
+CREATE PROCEDURE TypeQuestionStatsThisMonth()
 BEGIN
-    SELECT AccountID, Email, Username, FullName FROM `Account`
-    WHERE AccountID IN (
-        SELECT DepartmentID FROM `Department`
-        WHERE DepartmentName = DepartmentNameIn
-    );
+	SELECT T.TypeID, T.TypeName, COUNT(*) as NumQuestion, CONCAT(MONTH(NOW()), '-', YEAR(NOW())) as Time
+    FROM TypeQuestion T LEFT JOIN (
+        SELECT TypeID
+        FROM Question
+        WHERE MONTH(CreateDate) = MONTH(NOW()) AND YEAR(CreateDate) = YEAR(NOW())
+    ) Q
+        ON T.TypeID = Q.TypeID
+    GROUP BY T.TypeID;
 END $$
 
-CALL GetAccountsInDepartment('Engineer');
-DELIMITER ;
-
-DELIMITER $$
-CREATE PROCEDURE GetAccountPerGroup()
-BEGIN
-	SELECT G.GroupID as GID, G.GroupName as GroupName, COUNT(AccountID) as NumAccount
-    FROM `Group` G LEFT JOIN `GroupAccount` GA ON G.GroupID = GA.GroupID
-	GROUP BY G.GroupID;
-END $$
-
-CALL GetAccountPerGroup();
 
 DELIMITER ;
+
+
+CALL TypeQuestionStatsThisMonth();
+
